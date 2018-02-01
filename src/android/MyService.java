f47503a6a43d47a0b6b3f3ca824cec26da7db12d
pravinpsa <pravinpsa@gmail.com>
2018-02-01 15:32:28 +0530

    Add files via upload

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -7,13 +7,6 @@ import org.json.JSONObject;
 import android.util.Log;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 
 public class MyService extends BackgroundService {
 
@@ -33,10 +26,10 @@ public class MyService extends BackgroundService {
 	}
 
 	@Override
-	protected JSONObject doWork()  throws IOException {
+	protected JSONObject doWork() {
 	   JSONObject result = new JSONObject();
+			
 	   try {
-	   
 		  // Following three lines simply produce a text string with Hello World and the date & time (UK format)
 		  SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
 		  String now = df.format(new Date(System.currentTimeMillis())); 
@@ -45,41 +38,7 @@ public class MyService extends BackgroundService {
 		  // We output the message to the logcat
 		  Log.d("MyService", msg);
 
-		  
-		  
-
-		URL obj = new URL("http://192.168.1.11:8080/SSA/sampleReq");
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("POST");
-		con.setDoOutput(true);
-		OutputStream os = con.getOutputStream();
-		os.write("mobile=9500343485".getBytes());
-		os.flush();
-		os.close();
-
-
-		int responseCode = con.getResponseCode();
-		System.out.println("POST Response Code :: " + responseCode);
-
-		if (responseCode == HttpURLConnection.HTTP_OK) { //success
-			BufferedReader in = new BufferedReader(new InputStreamReader(	con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			System.out.println(response.toString());
-		} else {
-			System.out.println("POST request not worked");
-		}
-		  
-		  
-		  
-		  
-		  
-			  // We also provide the same message in our JSON Result
+		  // We also provide the same message in our JSON Result
 		  result.put("Message", msg);
 	   } catch (JSONException e) {
 		  // In production code, you would have some exception handling here
@@ -87,5 +46,4 @@ public class MyService extends BackgroundService {
 			
 	   return result;	
 	}
-
 }
